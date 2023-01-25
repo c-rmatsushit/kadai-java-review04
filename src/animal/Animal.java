3c41f83efe29e1799a0cd105546a405bd2366ba5
Ryunosuke Matsushita <c-rmatsushit@localhost>
2023-01-25 09:28:32 +0900

    ヒント記入

--- src/animal/Animal.java
+++ src/animal/Animal.java
@@ -1,5 +1,11 @@
 package animal;
 
 public class Animal {
+	  // フィールドなどの内容は省略。自力で考えてみましょう
 
-}
+
+	  // この部分を、コンストラクタを使った初期化の処理で埋めてみましょう
+	  public Animal() {
+
+	  }
+	}
--- src/animal/Review04.java
+++ src/animal/Review04.java
@@ -2,4 +2,8 @@ package animal;
 
 public class Review04 {
 
+    public static void main(String[] args) {
+        Animal animal = new Animal("田中 太郎", 25);
+        animal.say();
+    }
 }
